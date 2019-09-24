a87cf7d430ac3b36441b92d1bfe72fff19ffd691
helloWorld <531697348@qq.com>
2019-09-24 18:22:59 +0800

    修正接口返回中文乱码问题

--- mavenspring01/src/main/java/com/mywork/spring/controller/login/LoginController.java
+++ mavenspring01/src/main/java/com/mywork/spring/controller/login/LoginController.java
@@ -3,6 +3,8 @@ package com.mywork.spring.controller.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
